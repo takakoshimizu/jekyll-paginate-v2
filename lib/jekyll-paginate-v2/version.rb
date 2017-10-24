8926a6ef095ab13c5b4dc7449a0f06ff37730d70
Erich Dalton <erich.dalton@ozmoapp.com>
2017-10-24 10:32:24 -0400

    Update version

--- lib/jekyll-paginate-v2/version.rb
+++ lib/jekyll-paginate-v2/version.rb
@@ -1,6 +1,6 @@
 module Jekyll
   module PaginateV2
-    VERSION = "1.9.0"
+    VERSION = "1.9.1-ozmo"
     # When modifying remember to issue a new tag command in git before committing, then push the new tag
     #   git tag -a v1.9.0 -m "Gem v1.9.0"
     #   git push origin --tags
